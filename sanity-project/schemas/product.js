76dc448d3ebdaabd5a68504a4a4da10af849467e
Alameen8739 <allialameen8739@gmail.com>
2023-01-20 16:02:26 +0100

    created product schema

---
+++ sanity-project/schemas/product.js
@@ -0,0 +1,11 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    field: [
+        {
+            name: 'image',
+            title: ''
+        }
+    ] 
+}
